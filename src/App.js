5224439f1d08043806e72b0acd094d1f4674f9b6
Domodan <ongomdaniel9@gmail.com>
2023-01-17 15:57:57 +0300

    connecting holicar-front app to the repo

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to HoliCare
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Domodan/holicare-front"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Findout more Here
         </a>
       </header>
     </div>
