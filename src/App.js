f0c23a3a9585af2ef47afa0fccbde06e43a9ed73
Happy-Betty <happybetty1999@Gmail.com>
2023-02-24 12:56:35 +0300

    merged hapi into dev

--- src/App.js
+++ src/App.js
@@ -25,18 +25,9 @@ import Analytic from "./components/pages/analytics";
 import Calendar from "./components/pages/calendar";
 import Setting from "./components/pages/setting";
 import UserProfile from "./components/pages/userprofile";
-<<<<<<< HEAD
-import Layout from "./components/pages/layout/Layout";
-import RequireAuth from "./auth/requireAuth/RequireAuth";
-import SignIn from "./components/pages/auth/SignIn";
-import SignUp from "./components/pages/auth/SignUp";
-import OTPVerification from "./components/pages/auth/OTPVerification";
-import useAuth from "./auth/useAuth/useAuth";
-=======
 import  {Login} from "./components/pages/Login";
 import {SignUp}  from "./components/pages/signUp";
 import OtpForm from "./components/pages/otpForm";
->>>>>>> hapi
 
 const routes = [ "/", "/sign_in", "/sign_up", "/otp" ];
 
@@ -44,64 +35,6 @@ function App() {
     const [theme, colorMode] = useMode();
     const [isSidebar, setIsSidebar] = useState(true);
     const [landing, setLanding] = useState(true);
-<<<<<<< HEAD
-    const { authed } = useAuth();
-    const location = useLocation();
-    const pathname = location.pathname;
-
-    useEffect(() => {
-        if (authed) setLanding(false);
-        else setLanding(true);
-    }, [authed]);
-    
-    return (
-        <>{(landing && routes.includes(pathname))
-            ?
-                <Routes>
-                    <Route path="/" element={<Layout />}>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/sign_in" element={<SignIn />} />
-                        <Route path="/sign_up" element={<SignUp />} />
-                        <Route path="/otp" element={<OTPVerification />} />
-                    </Route>
-                </Routes>
-            :
-                <ColorModeContext.Provider value={ colorMode }>
-                    <ThemeProvider theme={ theme }>
-                        <CssBaseline />
-                        <div className="app">
-                            <Sidebar isSidebar={isSidebar} />
-                            <main className="content">
-                                <Topbar setIsSidebar={setIsSidebar} />
-                                <Routes>
-                                        <Route element={<RequireAuth />}>
-                                            <Route path="/dashboard" element={<Dashboard />} />
-                                            <Route path="/admin" element={<Team />} />
-                                            <Route path="/doctor" element={<Doctor />} />
-                                            <Route path="/patient" element={<Patient />} />
-                                            <Route path="/add-patient" element={<AddPatient />} />
-                                            <Route path="/hospital" element={<Hospital />} />
-                                            <Route path="/add-hospital" element={<AddHospital />} />
-                                            <Route path="/district" element={<District />} />
-                                            <Route path="/add-district" element={<AddDistrict />} />
-                                            <Route path="/lab" element={<Laboratory />} />
-                                            <Route path="/infection" element={<Infection />} />
-                                            <Route path="/add-infection" element={<AddInfection />} />
-                                            <Route path="/risk-factor" element={<RiskFactor />} />
-                                            <Route path="/add-riskfactor" element={<AddRiskfactor />} />
-                                            <Route path="/message" element={<Message />} />
-                                            <Route path="/test" element={<Tests />} />
-                                            <Route path="/analytic" element={<Analytic />} />
-                                            <Route path="/calendar" element={<Calendar />} />
-                                            <Route path="/setting" element={<Setting />} />
-                                            <Route path="/user-profile" element={<UserProfile />} />
-                                        </Route>
-                                </Routes>
-                            </main>
-                        </div>
-                    </ThemeProvider>
-                </ColorModeContext.Provider>
-=======
     const [login, setLogin] = useState(true);
     const location = useLocation();
     console.log("From:", location.state?.from?.pathname);
@@ -177,7 +110,6 @@ function App() {
                     </div>
                 </ThemeProvider>
             </ColorModeContext.Provider>
->>>>>>> hapi
             }
         </>
     )
