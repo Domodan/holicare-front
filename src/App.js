b050d08fe162ba79afe2365ef3b45b64a89dfb6b
Happy-Betty <happybetty1999@Gmail.com>
2023-03-16 15:31:44 +0300

    The veriffy your email page

--- src/App.js
+++ src/App.js
@@ -31,8 +31,8 @@ import SignIn from "./components/pages/auth/SignIn";
 import SignUp from "./components/pages/auth/SignUp";
 import OTPVerification from "./components/pages/auth/OTPVerification";
 import useAuth from "./auth/useAuth/useAuth";
-
-const routes = [ "/", "/sign_in", "/sign_up", "/otp" ];
+import {VerifyMail} from "./components/pages/verifyMail";
+const routes = [ "/", "/sign_in", "/sign_up", "/otp", "/verify"];
 
 function App() {
     const [theme, colorMode] = useMode();
@@ -56,6 +56,8 @@ function App() {
                         <Route path="/sign_in" element={<SignIn />} />
                         <Route path="/sign_up" element={<SignUp />} />
                         <Route path="/otp" element={<OTPVerification />} />
+                        <Route path="/verify" element={<VerifyMail />} />
+
                     </Route>
                 </Routes>
             :
