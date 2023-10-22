4a7135cf5032bf10039848b4af0da7707f9038c3
domodan <ongomdaniel9@gmail.com>
2023-10-22 23:50:28 +0300

    pointed the Base URL back to main

--- src/utils/GlobalVariables.jsx
+++ src/utils/GlobalVariables.jsx
@@ -1,8 +1,8 @@
 export const globalVariables = {
     // Request URLs
-    // BASE_URL: "https://holicare.herokuapp.com/api/v1/",
-    // BASE_URL_2: "http://196.43.134.157:8000/api/v1/",
-    BASE_URL: "http://196.43.134.157:8000/api/v1/",
+    BASE_URL: "https://holicare.herokuapp.com/api/v1/",
+    // BASE_URL: "http://196.43.134.157:8000/api/v1/",
+    BASE_URL_2: "http://196.43.134.157:8000/api/v1/",
     BASE_URL_3: "http://196.43.134.157:8000/",
 
     // Request Headers
